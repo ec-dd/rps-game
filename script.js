d39778d974afcec8a19910ba1a2d434ba9f17e31
ec-dd <125108159+ec-dd@users.noreply.github.com>
2023-03-04 17:39:51 -0500

    implemented 'playRound()' method

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
 // Function that returns either 'Rock', 'Paper' or 'Scissors'
 function getComputerChoice() {
-  let choices = ["Rock", "Paper", "Scissors"]; // Choices of rps
+  let choices = ["rock", "paper", "scissors"]; // Choices of rps
 
   //return selected choice randomly
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+// Function to play a round of rps
+function playRound(playerSelection, computerSelection) {
+  let result = "";
+  // When computer beats player
+  if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper")
+    result += "You Lose! Paper beats Rock";
+
+  if (
+    playerSelection.toLowerCase() === "scissprs" &&
+    computerSelection === "rock"
+  )
+    result += "You Lose! Rock beats Scissors";
+
+  if (
+    playerSelection.toLowerCase() === "paper" &&
+    computerSelection === "scissors"
+  )
+    result += "You Lose! Scissors beats Paper";
+
+  // When player beats computer
+  if (
+    playerSelection.toLowerCase() === "scissors" &&
+    computerSelection === "paper"
+  )
+    result += "You Win! Paper beats Rock";
+
+  if (
+    playerSelection.toLowerCase() === "rock" &&
+    computerSelection === "scissors"
+  )
+    result += "You Win! Rock beats Scissors";
+
+  if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock")
+    result += "You Win! Paper beats Rock";
+
+  if (
+    (playerSelection.toLowerCase() === "paper" &&
+      computerSelection === "paper") ||
+    (playerSelection.toLowerCase() === "rock" &&
+      computerSelection === "rock") ||
+    (playerSelection.toLowerCase() === "scissors" &&
+      computerSelection === "scissors")
+  ) {
+    result += "It's a tie";
+  }
+
+  return result;
 }
