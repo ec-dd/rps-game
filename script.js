3af9de1f4aef3039cc6883ff4074f17824b44bf0
ec-dd <125108159+ec-dd@users.noreply.github.com>
2023-03-07 22:43:37 -0500

    implemented event listener on buttons

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//Scorekeeping instances
+let playerScore = 0;
+let computerScore = 0;
+let score = "";
+
+//accessing html elements with DOM
+let result = document.querySelector("#result"); //div to display result
+const rockBtn = document.querySelector(".rock-btn"); // rock button
+const paperBtn = document.querySelector(".paper-btn"); // paper button
+const scissorsBtn = document.querySelector(".scissors-btn"); // scissors button
+
+// Click event on buttons (calling playRound())
+rockBtn.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+});
+
+paperBtn.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+
+scissorsBtn.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
+
 // Function that returns either 'Rock', 'Paper' or 'Scissors'
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"]; // Choices of rps
@@ -6,11 +30,6 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-let result = "";
-let playerScore = 0;
-let computerScore = 0;
-let score = "";
-
 // Function to play a round of rps
 function playRound(playerSelection, computerSelection) {
   // When computer beats player
@@ -18,24 +37,24 @@ function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase() === "rock" &&
     computerSelection === "paper"
   ) {
-    result = "You Lose! Paper beats Rock ";
-    computerScore++;
+    result.innerHTML = "You Lose! Paper beats Rock ";
+    // computerScore++;
   }
 
   if (
-    playerSelection.toLowerCase() === "scissprs" &&
+    playerSelection.toLowerCase() === "scissors" &&
     computerSelection === "rock"
   ) {
-    result = "You Lose! Rock beats Scissors ";
-    computerScore++;
+    result.innerHTML = "You Lose! Rock beats Scissors ";
+    // computerScore++;
   }
 
   if (
     playerSelection.toLowerCase() === "paper" &&
     computerSelection === "scissors"
   ) {
-    result = "You Lose! Scissors beats Paper ";
-    computerScore++;
+    result.innerHTML = "You Lose! Scissors beats Paper ";
+    // computerScore++;
   }
 
   // When player beats computer
@@ -43,24 +62,24 @@ function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase() === "scissors" &&
     computerSelection === "paper"
   ) {
-    result = "You Win! Paper beats Rock ";
-    playerScore++;
+    result.innerHTML = "You Win! Paper beats Rock ";
+    // playerScore++;
   }
 
   if (
     playerSelection.toLowerCase() === "rock" &&
     computerSelection === "scissors"
   ) {
-    result = "You Win! Rock beats Scissors ";
-    playerScore++;
+    result.innerHTML = "You Win! Rock beats Scissors ";
+    // playerScore++;
   }
 
   if (
     playerSelection.toLowerCase() === "paper" &&
     computerSelection === "rock"
   ) {
-    result = "You Win! Paper beats Rock ";
-    playerScore++;
+    result.innerHTML = "You Win! Paper beats Rock ";
+    // playerScore++;
   }
 
   // When players have the same choice, it's a tie
@@ -72,28 +91,28 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection.toLowerCase() === "scissors" &&
       computerSelection === "scissors")
   ) {
-    result = "It's a tie ";
+    result.innerHTML = "It's a tie ";
   }
 
   //   display score and winner of round
-  score = "Score is: Player " + playerScore + " vs Computer " + computerScore;
-  return result + score;
+  // score = "Score is: Player " + playerScore + " vs Computer " + computerScore;
+  return result.innerHTML;
 }
 
 // Function to generate a 5 round game of rps
-function game() {
-  //   Prompt user to prompt a choice for 5 rounds
-  for (let i = 0; i < 5; i++) {
-    let playerInput = prompt("rock, paper or scissors?");
-    console.log(playRound(playerInput, getComputerChoice()));
-  }
+// function game() {
+//   //   Prompt user to prompt a choice for 5 rounds
+//   for (let i = 0; i < 5; i++) {
+//     let playerInput = prompt("rock, paper or scissors?");
+//     console.log(playRound(playerInput, getComputerChoice()));
+//   }
 
-  //   Display the final score and winner
-  if (playerScore === computerScore) {
-    return "The game is tied! ";
-  }
+//   //   Display the final score and winner
+//   if (playerScore === computerScore) {
+//     return "The game is tied! ";
+//   }
 
-  return playerScore < computerScore
-    ? "You Lost! " + score
-    : "You Won! " + score;
-}
+//   return playerScore < computerScore
+//     ? "You Lost! " + score
+//     : "You Won! " + score;
+// }
